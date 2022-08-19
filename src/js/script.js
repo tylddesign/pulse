45128084f7b7fc8b3330afb7b43852cfb37f83eb
TYLD <vladimirovich.alex.gor@gmail.com>
2022-08-19 08:09:54 +0300

    Убрал Слик карусель, поставил Tiny. Дбавил верстку без кода по табам

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,32 @@
-$(document).ready(function () {
-    $('.carousel__inner').slick({
-        speed: 1200,
-        // adptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>'
-    });
+// $(document).ready(function () {
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         // adptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>'
+//     });
+// });
+
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+    // prevButton: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>'
+});
+
+// document.querySelector('.slick-prev').onclick = function () {
+//     slider.goTo('prev');
+// };
+
+
+document.querySelector('.slick-prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.slick-next').addEventListener('click', function () {
+    slider.goTo('next');
 });
