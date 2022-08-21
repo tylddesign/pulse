aaf190f89802323d172caeb32fca9530d9a387d3
Alexey Goryainov <vladimirovich.alex.gor@gmail.com>
2022-08-21 20:05:09 +0200

    Добавил айтемы со стилями. Стилят содержат подготову к анимации переключения информации о товаре. Добавлен скрипт к переключению анимации. Добавлены шаблонные айтемы в контент для будущих табов. Добавил код к слайдеру и комментарии к переключению контента

--- src/js/script.js
+++ src/js/script.js
@@ -30,3 +30,25 @@ document.querySelector('.slick-prev').addEventListener('click', function () {
 document.querySelector('.slick-next').addEventListener('click', function () {
     slider.goTo('next');
 });
+
+$(document).ready(function () {
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active'); // closest - ближайший элемент (контейнер), внутри него find - найти активный контент, removeClass - удалить класс активности, eq - получает тот номер элемента на который мы кликнули, addClass - добавляет класс.
+    });
+
+    function toggleSlide(item) { // функция "переключатель"
+        $(item).each(function (i) { //каждый элемент (item) который попал в функцию будет делать что-то
+            $(this).on('click', function (e) { //this - текущий выбранный элемент по клику будет делать следующую функцию
+                e.preventDefault(); //убираем стандартное поведение браузера у ссылки
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active'); // при клике на эту кнопку будет переключаться(toggle) класс. Eq - позволяет получать элемент по индексу
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active'); // при клике на эту кнопку будет переключаться(toggle) класс. Eq - позволяет получать элемент по индексу
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
